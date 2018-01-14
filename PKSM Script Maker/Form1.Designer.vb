0219ac34124eecca292fef02404705911e7c2bc4
Regnum <nkochen@hotmail.com>
2018-01-13 21:15:00 -0600

    Initial Commit

---
+++ PKSM Script Maker/Form1.Designer.vb
@@ -0,0 +1,342 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Comic Sans MS", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(26, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(230, 66)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Open WC7FULL"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Comic Sans MS", 14.0!, System.Drawing.FontStyle.Bold)
+        Me.Button2.Location = New System.Drawing.Point(26, 161)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(230, 66)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Save WC7"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(254, 214)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(26, 19)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Comic Sans MS", 14.0!, System.Drawing.FontStyle.Bold)
+        Me.Button3.Location = New System.Drawing.Point(26, 83)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(230, 66)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Convert"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Location = New System.Drawing.Point(-3, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(291, 265)
+        Me.TabControl1.TabIndex = 4
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.SystemColors.Control
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.TextBox4)
+        Me.TabPage1.Controls.Add(Me.Button5)
+        Me.TabPage1.Controls.Add(Me.Button4)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.TextBox3)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.TextBox2)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.TextBox1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(283, 239)
+        Me.TabPage1.TabIndex = 1
+        Me.TabPage1.Text = "PKSM Script Maker"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 80)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(43, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Length:"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(6, 96)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(259, 20)
+        Me.TextBox4.TabIndex = 8
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(103, 204)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(63, 23)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "Finish"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(202, 132)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(63, 23)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Browse"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Data:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(6, 134)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(191, 20)
+        Me.TextBox3.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 42)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Offset:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(6, 58)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(259, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 4)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Script Name:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 20)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(259, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TabPage2
+        '
+        Me.TabPage2.BackColor = System.Drawing.SystemColors.Control
+        Me.TabPage2.Controls.Add(Me.Button1)
+        Me.TabPage2.Controls.Add(Me.Button3)
+        Me.TabPage2.Controls.Add(Me.Button2)
+        Me.TabPage2.Controls.Add(Me.RichTextBox1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(283, 239)
+        Me.TabPage2.TabIndex = 0
+        Me.TabPage2.Text = "WC7FULL to WC7"
+        '
+        'TabPage3
+        '
+        Me.TabPage3.BackColor = System.Drawing.SystemColors.Control
+        Me.TabPage3.Controls.Add(Me.Button6)
+        Me.TabPage3.Controls.Add(Me.ComboBox3)
+        Me.TabPage3.Controls.Add(Me.Label5)
+        Me.TabPage3.Controls.Add(Me.ComboBox2)
+        Me.TabPage3.Controls.Add(Me.ComboBox1)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Size = New System.Drawing.Size(283, 239)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Info"
+        '
+        'Button6
+        '
+        Me.Button6.Enabled = False
+        Me.Button6.Location = New System.Drawing.Point(79, 112)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(125, 23)
+        Me.Button6.TabIndex = 4
+        Me.Button6.Text = "Copy to Script Maker"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"Sun/Moon", "UltraSun/UltraMoon"})
+        Me.ComboBox3.Location = New System.Drawing.Point(86, 10)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(104, 21)
+        Me.ComboBox3.TabIndex = 3
+        Me.ComboBox3.Text = "--Game--"
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(12, 72)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(259, 23)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = " "
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.Enabled = False
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.ComboBox2.Location = New System.Drawing.Point(193, 40)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(78, 21)
+        Me.ComboBox2.TabIndex = 1
+        Me.ComboBox2.Text = "--Slot #--"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Enabled = False
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Wonder Card Slot"})
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 40)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(175, 21)
+        Me.ComboBox1.TabIndex = 0
+        Me.ComboBox1.Text = "-------Offsets-------"
+        '
+        'OpenFileDialog2
+        '
+        Me.OpenFileDialog2.FileName = "OpenFileDialog2"
+        '
+        'Form1
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "PKSM Script Maker"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button2 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents OpenFileDialog2 As OpenFileDialog
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Button6 As Button
+End Class
---
+++ PKSM Script Maker/Form1.vb
@@ -0,0 +1,261 @@
+﻿Public Class Form1
+    Dim pathexe = My.Application.Info.DirectoryPath
+    Dim path = pathexe & "\assets"
+    Dim web As New System.Net.WebClient
+    Dim prog As String = pathexe & "\PKSMScript.py"
+    Dim sm As String
+    Dim data(3) As String
+
+    Dim stat As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim Everest_Registry As Microsoft.Win32.RegistryKey = My.Computer.Registry.CurrentUser.OpenSubKey("Software\Python\PythonCore")
+        If Everest_Registry Is Nothing Then
+            'key does not exist
+            Dim py As Integer = MsgB("Python 3+ is required for script creation. Would you like to get python or continue without script creation?", 2, "Get Python", "Continue Anyway", "", "Python Required")
+            If py = 6 Then
+                Dim webAddress As String = "https://www.python.org/downloads/"
+                Process.Start(webAddress)
+                Application.Exit()
+            ElseIf py = 7 Then
+                TabPage1.Enabled = False
+            End If
+        Else
+        End If
+
+        TabControl1.TabIndex += 1
+        RichTextBox1.Hide()
+        buttons()
+        TabControl1.TabIndex -= 1
+    End Sub
+    Private Function MsgB(ByVal mes As String, ByVal numB As Integer, ByVal But1 As String, ByVal But2 As String, ByVal But3 As String, ByVal head As String)
+        Dim msg As New CustomMessageBox(mes, numB, But1, But2, But3, head)
+        Dim result = msg.ShowDialog()
+        Dim Ans As Integer
+        If result = Windows.Forms.DialogResult.Yes Then
+            'user clicked "B1"
+            Ans = 6
+        ElseIf result = Windows.Forms.DialogResult.No Then
+            'user clicked "B2"
+            Ans = 7
+        ElseIf result = Windows.Forms.DialogResult.Cancel Then
+            'user clicked "B3"
+            Ans = 8
+        Else
+            'user closed the window without clicking a button
+            Ans = -1
+            Close()
+        End If
+        Return Ans
+    End Function 'custom MsgBox
+
+    'WC7FULL to WC7
+    Private Shared Function HexStringToByteArray(ByRef strInput As String) As Byte()
+        Dim length As Integer
+        Dim bOutput As Byte()
+        Dim c(1) As Integer
+        length = strInput.Length / 2
+        ReDim bOutput(length - 1)
+        For i As Integer = 0 To (length - 1)
+            For j As Integer = 0 To 1
+                c(j) = Asc(strInput.Chars(i * 2 + j))
+                If ((c(j) >= Asc("0")) And (c(j) <= Asc("9"))) Then
+                    c(j) = c(j) - Asc("0")
+                ElseIf ((c(j) >= Asc("A")) And (c(j) <= Asc("F"))) Then
+                    c(j) = c(j) - Asc("A") + &HA
+                ElseIf ((c(j) >= Asc("a")) And (c(j) <= Asc("f"))) Then
+                    c(j) = c(j) - Asc("a") + &HA
+                End If
+            Next j
+            bOutput(i) = (c(0) * &H10 + c(1))
+        Next i
+        Return (bOutput)
+    End Function
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        OpenFileDialog1.Filter = "wc7full files (*.wc7full)|*.wc7full|All files (*.*)|*.*"
+        OpenFileDialog1.ShowDialog()
+        Dim myFile As String = OpenFileDialog1.FileName
+        Dim ext() As String = myFile.Split(".")
+
+        If ext(UBound(ext)) = "wc7full" Or ext(UBound(ext)) = "WC7FULL" Then
+            'Dim myFile As String = "..\..\0264 USUM - Item Roto Catch x1 (JPN).wc7full"
+            Dim myBytes As Byte() = My.Computer.FileSystem.ReadAllBytes(myFile)
+            Dim txtTemp As New System.Text.StringBuilder()
+            For Each myByte As Byte In myBytes
+                txtTemp.Append(myByte.ToString("X2"))
+            Next
+            RichTextBox1.Text = txtTemp.ToString()
+
+            stat = 1
+            buttons()
+        Else
+            MsgBox("Not a WC7FULL file", MsgBoxStyle.OkOnly)
+        End If
+    End Sub
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        SaveFileDialog1.Filter = "wc7 files (*.wc7)|*.wc7|All files (*.*)|*.*"
+        SaveFileDialog1.ShowDialog()
+        Dim myFile As String = SaveFileDialog1.FileName
+
+        'Dim myFile As String = "..\..\0264 USUM - Item Roto Catch x1 (JPN) 2.wc7"
+        Dim myBytes As Byte() = HexStringToByteArray(RichTextBox1.Text)
+        My.Computer.FileSystem.WriteAllBytes(myFile, myBytes, False)
+
+        stat = 0
+        buttons()
+    End Sub
+    Private Sub cut()
+        Dim SizeWC = 528
+        Dim SizeFull = 1568
+        Dim data As String = RichTextBox1.Text
+        Debug.Print(data.Length)
+        If (data.Length = SizeFull) Then
+            data = data.Skip(SizeFull - SizeWC).ToArray()
+        End If
+        RichTextBox1.Text = data
+        MsgBox("Successfully Converted", MsgBoxStyle.OkOnly)
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        cut()
+        stat = 2
+        buttons()
+    End Sub
+    Private Sub buttons()
+        If stat = 0 Then
+            Button1.Enabled = True
+            Button2.Enabled = False
+            Button3.Enabled = False
+        ElseIf stat = 1 Then
+            Button1.Enabled = True
+            Button2.Enabled = False
+            Button3.Enabled = True
+        ElseIf stat = 2 Then
+            Button1.Enabled = True
+            Button2.Enabled = True
+            Button3.Enabled = False
+        End If
+    End Sub
+
+    'Script Maker
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Then
+            MsgBox("Missing Value(s)", MsgBoxStyle.OkOnly)
+        Else
+            System.IO.File.WriteAllText(prog, My.Resources.PKSMScript)
+            ' web.DownloadFileAsync(New Uri("https://github.com/BernardoGiordano/PKSM-Tools/raw/master/PKSMScript/PKSMScript.py"), prog)
+            data(0) = TextBox1.Text
+            If TextBox2.Text.Contains("0x") Then
+                data(1) = TextBox2.Text
+            Else
+                data(1) = "0x" & TextBox2.Text
+            End If
+            If TextBox4.Text.Contains("0x") Then
+                data(2) = TextBox4.Text
+            Else
+                data(2) = "0x" & TextBox4.Text
+            End If
+            data(3) = TextBox3.Text
+            If data(3).Contains("C:\") Then
+                Dim da() As String = data(3).Split("\")
+                da(UBound(da)) = da(UBound(da)).Replace(" ", "_")
+                If System.IO.File.Exists(pathexe & "\" & da(UBound(da))) Then
+                Else
+                    System.IO.File.Copy(data(3), pathexe & "\" & da(UBound(da)))
+                End If
+                Dim d2 As String = da(UBound(da))
+                sm = "@py -3 PKSMScript.py """ & data(0) & """ -i " & data(1) & " " & data(2) & " " & """" & d2 & """" & " 1
+@PUSHD %1
+@MOVE *.pksm output
+@del PKSMScript.py
+@del " & da(UBound(da)) & "
+@del output.bat"
+            Else
+                sm = "@py -3 PKSMScript.py """ & data(0) & """ -i " & data(1) & " " & data(2) & " " & data(3) & " 1
+@PUSHD %1
+@MOVE *.pksm output
+@del PKSMScript.py
+@del output.bat"
+            End If
+            System.IO.File.WriteAllText(pathexe & "\output.bat", sm)
+            My.Computer.FileSystem.CreateDirectory(pathexe & "\output")
+            Process.Start(pathexe & "\output.bat")
+            System.Threading.Thread.Sleep(1000)
+            MsgBox("Done", 0)
+        End If
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        OpenFileDialog2.Filter = "wc7 files (*.wc7)|*.wc7|bin files (*.bin)|*.bin|txt files (*.txt)|*.txt|All files (*.*)|*.*"
+        OpenFileDialog2.ShowDialog()
+        TextBox3.Text = OpenFileDialog2.FileName
+    End Sub
+
+    'Info
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.Text = "Wonder Card Slot" Then
+            ComboBox2.Enabled = True
+        Else
+            ComboBox2.Enabled = False
+        End If
+    End Sub
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        If ComboBox2.Text = "1" And ComboBox3.Text = "Sun/Moon" Then
+            Label5.Text = "0x65D00"
+        ElseIf ComboBox2.Text = "2" And ComboBox3.Text = "Sun/Moon" Then
+            Label5.Text = "0x65E08"
+        ElseIf ComboBox2.Text = "3" And ComboBox3.Text = "Sun/Moon" Then
+            Label5.Text = "0x65F10"
+        ElseIf ComboBox2.Text = "4" And ComboBox3.Text = "Sun/Moon" Then
+            Label5.Text = "0x66018"
+        ElseIf ComboBox2.Text = "5" And ComboBox3.Text = "Sun/Moon" Then
+            Label5.Text = "0x66120"
+        ElseIf ComboBox2.Text = "6" And ComboBox3.Text = "Sun/Moon" Then
+            Label5.Text = "0x66228"
+        ElseIf ComboBox2.Text = "7" And ComboBox3.Text = "Sun/Moon" Then
+            Label5.Text = "0x66330"
+        ElseIf ComboBox2.Text = "8" And ComboBox3.Text = "Sun/Moon" Then
+            Label5.Text = "0x66438"
+        ElseIf ComboBox2.Text = "9" And ComboBox3.Text = "Sun/Moon" Then
+            Label5.Text = "0x66540"
+        ElseIf ComboBox2.Text = "10" And ComboBox3.Text = "Sun/Moon" Then
+            Label5.Text = "0x66648"
+
+        ElseIf ComboBox2.Text = "1" And ComboBox3.Text = "UltraSun/UltraMoon" Then
+            Label5.Text = "0x66300"
+        ElseIf ComboBox2.Text = "2" And ComboBox3.Text = "UltraSun/UltraMoon" Then
+            Label5.Text = "0x66408"
+        ElseIf ComboBox2.Text = "3" And ComboBox3.Text = "UltraSun/UltraMoon" Then
+            Label5.Text = "0x66510"
+        ElseIf ComboBox2.Text = "4" And ComboBox3.Text = "UltraSun/UltraMoon" Then
+            Label5.Text = "0x66618"
+        ElseIf ComboBox2.Text = "5" And ComboBox3.Text = "UltraSun/UltraMoon" Then
+            Label5.Text = "0x66720"
+        ElseIf ComboBox2.Text = "6" And ComboBox3.Text = "UltraSun/UltraMoon" Then
+            Label5.Text = "0x66828"
+        ElseIf ComboBox2.Text = "7" And ComboBox3.Text = "UltraSun/UltraMoon" Then
+            Label5.Text = "0x66930"
+        ElseIf ComboBox2.Text = "8" And ComboBox3.Text = "UltraSun/UltraMoon" Then
+            Label5.Text = "0x66A38"
+        ElseIf ComboBox2.Text = "9" And ComboBox3.Text = "UltraSun/UltraMoon" Then
+            Label5.Text = "0x66B40"
+        ElseIf ComboBox2.Text = "10" And ComboBox3.Text = "UltraSun/UltraMoon" Then
+            Label5.Text = "0x66C48"
+        End If
+
+    End Sub
+    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
+        ComboBox1.Enabled = True
+        ComboBox2_SelectedIndexChanged(sender, e)
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        TabControl1.TabIndex -= 2
+        TextBox2.Text = Label5.Text
+        If ComboBox1.Text = "Wonder Card Slot" Then
+            TextBox4.Text = "0x108"
+        End If
+        TabControl1.TabIndex += 2
+    End Sub
+    Private Sub Label5_TextChanged(sender As Object, e As EventArgs) Handles Label5.TextChanged
+        Button6.Enabled = True
+    End Sub
+End Class
